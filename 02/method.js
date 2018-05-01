595fcb08d52de273a1f6e2cbda9d890d155f8eda
harapeko <github@harapeko.me>
2018-05-02 03:05:48 +0900

    add method

---
+++ 02/method.js
@@ -0,0 +1,22 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    current: new Date().toLocaleString()
+  },
+  computed: {
+    // 算出プロパティ経由で乱数を取得
+    randomc: function() {
+      return Math.random()
+    }
+  },
+  methods: {
+    // クリック時に処理を実行(詳細はPart3)
+    onclick: function() {
+      this.current = new Date().toLocaleString()
+    },
+    // メソッド経由で乱数を取得
+    randomm: function() {
+      return Math.random()
+    }
+  }
+})
